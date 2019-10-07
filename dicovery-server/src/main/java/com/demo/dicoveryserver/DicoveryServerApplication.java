d66b3da23c8f4c13fcf694ccfe321a69cb716d60
Yuheng Wang <yuheng.wang@servian.com>
2019-10-08 09:40:50 +1100

    init customer-service, api-gateway and dicovery-server

---
+++ dicovery-server/src/main/java/com/demo/dicoveryserver/DicoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dicoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoveryServerApplication.class, args);
+	}
+
+}
